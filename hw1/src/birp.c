f4f1d66e8f23e9521632d107a97c46f5764f5fe2
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-02-09 17:34:14 +0000

    lay out the modification requirement for global_option

--- hw1/src/birp.c
+++ hw1/src/birp.c
@@ -53,6 +53,7 @@ int validargs(int argc, char **argv) {
 
     // check if the first flag is -h 
     global_options = HELP_OPTION;
+    // global_options: bit 31 is 1 -> 0x80000000 (1000...0000)
 
     // loop through the flags to check
     // if the first flag is not -h then the max # of flags is 6
@@ -64,12 +65,33 @@ int validargs(int argc, char **argv) {
     // -i and -o can only appear at most once each
     // -i pgm|birp default birp
     // -o pgm|birp|ascii default birp
-    
+        // -i is bits 0-3 0x1234567_ 
+            // 0000 (0x0) is not allowed
+            // 0001 (0x1) for pgm
+            // 0010 (0x2) for birp
+        // -i is bits 4-8 0x123456_8
+            // 0000 (0x0) is not allowed
+            // 0001 (0x1) for pgm
+            // 0010 (0x2) for birp
+            // 0011 (0x3) for ascii 
+
     // if input and output format are both birp
+        // global option is 0x00000022 
     // at most one of following is allow
     // -n|-r|-t|-z|-Z 
     // for -t the next flag must be an integer in the range [0,255]
     // for -z & -Z the next flag must be an integer in the range [0,16]
+
+    // transformation is bits 8-11 0x12345_78
+        // 0000 (0x0) is no transformation
+        // 0001 (0x1) is -n 
+        // 0010 (0x2) is -t
+        // 0011 (0x3) is -z/-Z
+        // 0100 (0x4) is -r
+    // value for transformation is bits 16-23 0x12__5678
+        // negative for -z (twos complement)
+        // positive for -Z (twos complement)
+        // positive for -t (unsigned) [0,255]
 
     // for (int index = 0; index < argc; index++) {
         // 
