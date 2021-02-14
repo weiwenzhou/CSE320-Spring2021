98cda4cf52513a6f1e2df7f064c17eae006887c4
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-02-14 22:50:23 +0000

    check global option and call the proper input to output function

--- hw1/src/main.c
+++ hw1/src/main.c
@@ -3,6 +3,9 @@
 
 #include "const.h"
 #include "debug.h"
+#include "bdd.h"
+#include "custom_functions.h"
+#include "image.h"
 
 #ifdef _STRING_H
 #error "Do not #include <string.h>. You will get a ZERO."
@@ -18,11 +21,50 @@
 
 int main(int argc, char **argv)
 {
-    if(validargs(argc, argv))
+    if(validargs(argc, argv)) {
+        debug("exit failure");
+        debug("0x%X",global_options);
         USAGE(*argv, EXIT_FAILURE);
-    if(global_options & HELP_OPTION)
+    }
+    // Placeholder for the checking value
+    debug("0x%X",global_options);
+    
+    if(global_options & HELP_OPTION) {
+        debug("exit success");
         USAGE(*argv, EXIT_SUCCESS);
+    }
     // TO BE IMPLEMENTED
+    if (global_options == 0x21) {
+        if (pgm_to_birp(stdin, stdout))
+            return EXIT_FAILURE;
+        else 
+            return EXIT_SUCCESS;
+    }
+    if (global_options == 0x12) {
+        if (birp_to_pgm(stdin, stdout))
+            return EXIT_FAILURE;
+        else
+            return EXIT_SUCCESS;
+    }
+    if (global_options == 0x22) {
+        if (birp_to_birp(stdin, stdout))
+            return EXIT_FAILURE;
+        else
+            return EXIT_SUCCESS;
+    }
+    if (global_options == 0x31) {
+        if (pgm_to_ascii(stdin, stdout))
+            return EXIT_FAILURE;
+        else 
+            return EXIT_SUCCESS;
+    }
+    if (global_options == 0x32) {
+        if (birp_to_ascii(stdin, stdout))
+            return EXIT_FAILURE;
+        else
+            return EXIT_SUCCESS;
+    }
+    debug("TBA");
     return EXIT_FAILURE;
 }
 
