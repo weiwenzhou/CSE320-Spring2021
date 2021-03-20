42cadd5bdd4448c8c1e3efcb12a1c23259e01346
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-03-20 21:59:24 +0000

    first malloc call initialize heap

--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -7,8 +7,14 @@
 #include <string.h>
 #include "debug.h"
 #include "sfmm.h"
+#include "custom_functions.h"
 
 void *sf_malloc(size_t size) {
+    sf_block *start = (sf_block *) sf_mem_start();
+    sf_block *end = (sf_block *) sf_mem_end();
+    if (start == end) {
+        sf_initialize_heap();
+    }
     return NULL;
 }
 
