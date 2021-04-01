45fcb30b8b222e71aa222548312b9127b3ea8b9c
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-04-01 00:32:39 +0000

    helper function to split a string into an array

---
+++ hw4/include/custom_functions.h
@@ -0,0 +1,11 @@
+/**
+ * Splits a string using whitespaces as the delimiter. The 
+ * strings in the array do not have whitespaces.
+ * 
+ * @param string The string to split
+ * @param length The int pointer to update to the length 
+ * 
+ * @return NULL if the string is empty else an array of the strings 
+ * split by whitespaces.
+ */
+char **split_string(char *string, int *length);
---
+++ hw4/src/custom_functions.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "custom_functions.h"
+#include "debug.h"
+
+char **split_string(char *string, int *length) {
+    if (strlen(string) == 0) 
+        return NULL;
+    int length_temp = 1;
+    char *updated = malloc(strlen(string)+1);
+    strcpy(updated, string);
+    char *token = strtok(updated, " ");
+    // get the length of the array
+    while ((token = strtok(NULL, " ")) != NULL) 
+        length_temp++;
+    // create an array of the proper
+    char **array = calloc(length_temp, sizeof(char *));
+    *length = length_temp;
+    length_temp = 0;
+    token = strtok(string, " ");
+    array[length_temp++] = token;
+    while ((token = strtok(NULL, " ")) != NULL) 
+        array[length_temp++] = token;
+    free(updated);
+    return array;
+}
