78528408aa2e21af574cbd594ad2fdcccfcf1f6a
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-04-01 17:04:19 +0000

    define printer struct and array/counter to keep track of printers

--- hw4/include/custom_functions.h
+++ hw4/include/custom_functions.h
@@ -1,5 +1,25 @@
+#include <stdio.h>
+
+#include "imprimer.h"
+
 #define WRONG_ARG_COUNT(length, required) printf("Wrong number of args (given: %d, required: %d) for CLI command 'help'\n", length, required); sf_cmd_error("arg count");
 #define CHECK_ARG(length, required) if (length != required) {WRONG_ARG_COUNT(length, required); goto bad_arg;}
+
+/*
+ * Structure that describes a printer.
+ */
+typedef struct printer {
+    char *name;
+    FILE_TYPE *type;
+    PRINTER_STATUS status;
+} PRINTER;
+
+/*
+ * Space to store the PRINTERs.
+ */
+PRINTER printers[MAX_PRINTERS];
+// Global counter to keep track of the number of printers
+int printer_count;
 
 /**
  * Splits a string using whitespaces as the delimiter. The 
