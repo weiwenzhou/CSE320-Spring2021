87fbbfac0f07f7b38de8e7f3edc9b4b5612c99fa
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-04-01 01:07:07 +0000

    start setup for the command checking

--- hw4/src/cli.c
+++ hw4/src/cli.c
@@ -11,6 +11,7 @@
 #include "imprimer.h"
 #include "conversions.h"
 #include "sf_readline.h"
+#include "custom_functions.h"
 #include "debug.h"
 
 int run_cli(FILE *in, FILE *out)
@@ -18,34 +19,58 @@ int run_cli(FILE *in, FILE *out)
     // TO BE IMPLEMENTED
     // int stdin_copy = dup(STDIN_FILENO);
     // int fd_in = fileno(in);
-    char *prompt = (in == stdin) ? "imp> ":"";
     // dup2(fd_in, STDIN_FILENO);
+    char *prompt = (in == stdin) ? "imp> ":"";
     while (1) {
-        char *cmd = sf_readline(prompt);
-        char *token = strtok(cmd, " ");
-        int args = -1;
-        while (token != NULL) {
-            args++;
-            token = strtok(NULL, " ");
-        }
-        info("%d, %s", args, strtok(cmd, " "));
-        
-        // info("|%s|", cmd);
+        char *cmd = sf_readline(prompt);        
+        int length = 0;
+        char **array = split_string(cmd, &length);
+
+        // decrement length to account for command
+        length--;
         if (cmd == NULL)
             break;
-        else if (strcmp(strtok(cmd, " "), "help") == 0) {
-            printf("%s", strtok(cmd, " "));
+        else if (strcmp(*array, "help") == 0) {
+            CHECK_ARG(length, 0);
             printf("Commands are: help quit type printer conversion printers jobs print cancel disable enable pause resume\n");
             sf_cmd_ok();
-        } else if (strcmp(cmd, "quit") == 0) {
-            sf_cmd_ok();
+        } else if (strcmp(*array, "quit") == 0) {
+            CHECK_ARG(length, 0);
             free(cmd);
+            free(array);
+            sf_cmd_ok();
             return -1;
-        } else if (strcmp(cmd, "type") == 0) {
+        } else if (strcmp(*array, "type") == 0) {
             
+        } else if (strcmp(*array, "printer") == 0) {
+            
+        } else if (strcmp(*array, "conversion") == 0) {
+            
+        } else if (strcmp(*array, "printers") == 0) {
+            
+        } else if (strcmp(*array, "jobs") == 0) {
+            
+        } else if (strcmp(*array, "print") == 0) {
+            
+        } else if (strcmp(*array, "cancel") == 0) {
+            
+        } else if (strcmp(*array, "pause") == 0) {
+            
+        } else if (strcmp(*array, "resume") == 0) {
+            
+        } else if (strcmp(*array, "disable") == 0) {
+            
+        } else if (strcmp(*array, "enable") == 0) {
+            
+        } else {
+            printf("Unrecognized command: %s\n", *array);
+            sf_cmd_error("unrecognized command");
         }
-
-        free(cmd);
+        
+        bad_arg:
+            free(cmd);
+            free(array);
+            
     }
     // fprintf(stderr, "You have to implement run_cli() before the application will function.\n");
     // abort();
