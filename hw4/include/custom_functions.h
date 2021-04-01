69bb30aef7f051fda6ffefa8db2ba2c168258efc
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-04-01 01:07:30 +0000

    define macro to help with command argument checking

--- hw4/include/custom_functions.h
+++ hw4/include/custom_functions.h
@@ -1,3 +1,6 @@
+#define WRONG_ARG_COUNT(length, required) printf("Wrong number of args (given: %d, required: %d) for CLI command 'help'\n", length, required); sf_cmd_error("arg count");
+#define CHECK_ARG(length, required) if (length != required) {WRONG_ARG_COUNT(length, required); goto bad_arg;}
+
 /**
  * Splits a string using whitespaces as the delimiter. The 
  * strings in the array do not have whitespaces.
