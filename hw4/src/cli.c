cc5c72d3bb6474fca9ec075a93d0e5dc331f8d54
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-04-11 06:25:49 +0000

    general clean up of code

--- hw4/src/cli.c
+++ hw4/src/cli.c
@@ -52,6 +52,7 @@ int run_cli(FILE *in, FILE *out)
             CHECK_ARG(length, 0);
             printf("Commands are: help quit type printer conversion printers jobs print cancel disable enable pause resume\n");
             sf_cmd_ok();
+
         } else if (strcmp(*array, "quit") == 0) {
             CHECK_ARG(length, 0);
             free(cmd);
@@ -59,12 +60,14 @@ int run_cli(FILE *in, FILE *out)
             sf_cmd_ok();
             returnValue = -1;
             break;
+            
         } else if (strcmp(*array, "type") == 0) {
             CHECK_ARG(length, 1);
             if (define_type(array[1]) == NULL)
                 sf_cmd_error("type");
             else
                 sf_cmd_ok();
+            
         } else if (strcmp(*array, "printer") == 0) {
             CHECK_ARG(length, 2);
             FILE_TYPE *type;
@@ -88,6 +91,7 @@ int run_cli(FILE *in, FILE *out)
             printf("PRINTER: id=%ld, name=%s, type=%s, status=%s\n", printer-printers, printer->name, printer->type->name, printer_status_names[printer->status]);
 
             sf_cmd_ok();
+
         } else if (strcmp(*array, "conversion") == 0) {
             if (length <= 3)
                 CHECK_ARG(length, 3);
@@ -106,11 +110,13 @@ int run_cli(FILE *in, FILE *out)
                 sf_cmd_error("conversion");
             else
                 sf_cmd_ok();
+
         } else if (strcmp(*array, "printers") == 0) {
             CHECK_ARG(length, 0);
             for (int i = 0; i < printer_count; i++)
                 printf("PRINTER: id=%d, name=%s, type=%s, status=%s\n", i, printers[i].name, printers[i].type->name, printer_status_names[printers[i].status]);
             sf_cmd_ok();
+
         } else if (strcmp(*array, "jobs") == 0) {
             CHECK_ARG(length, 0);
             for (int i = 0; i < MAX_JOBS; i++) {
@@ -119,6 +125,7 @@ int run_cli(FILE *in, FILE *out)
                 }
             }
             sf_cmd_ok();
+
         } else if (strcmp(*array, "print") == 0) {
             if (length <= 1)
                 CHECK_ARG(length, 1);
@@ -148,6 +155,7 @@ int run_cli(FILE *in, FILE *out)
             printf("JOB[%ld]: status=%s, eligible=%08x, file=%s\n", job-jobs, job_status_names[job->status], job->eligible, job->file);
             sf_job_created(job-jobs, job->file, job->type->name);
             sf_cmd_ok();
+
         } else if (strcmp(*array, "cancel") == 0) {
             CHECK_ARG(length, 1);
             int job_id = atoi(array[1]);
@@ -156,10 +164,11 @@ int run_cli(FILE *in, FILE *out)
                 sf_cmd_error("pause - invalid job number");
                 goto bad_arg;
             }
-            info("id %d pid: %d sig term", job_id, job_pids[job_id]);
+            // info("id %d pid: %d sig term", job_id, job_pids[job_id]);
             killpg(job_pids[job_id], SIGTERM);
             killpg(job_pids[job_id], SIGCONT);
             sf_cmd_ok();
+            
         } else if (strcmp(*array, "pause") == 0) {
             CHECK_ARG(length, 1);
             int job_id = atoi(array[1]);
@@ -168,9 +177,10 @@ int run_cli(FILE *in, FILE *out)
                 sf_cmd_error("pause - invalid job number");
                 goto bad_arg;
             }
-            info("id %d pid: %d", job_id, job_pids[job_id]);
+            // info("id %d pid: %d", job_id, job_pids[job_id]);
             killpg(job_pids[job_id], SIGSTOP);
             sf_cmd_ok();
+
         } else if (strcmp(*array, "resume") == 0) {
             CHECK_ARG(length, 1);
             int job_id = atoi(array[1]);
@@ -179,9 +189,10 @@ int run_cli(FILE *in, FILE *out)
                 sf_cmd_error("resume - invalid job number");
                 goto bad_arg;
             }
-            info("id %d pid: %d", job_id, job_pids[job_id]);
+            // info("id %d pid: %d", job_id, job_pids[job_id]);
             killpg(job_pids[job_id], SIGCONT);
             sf_cmd_ok();
+
         } else if (strcmp(*array, "disable") == 0) {
             CHECK_ARG(length, 1);
             PRINTER *printer = find_printer_name(array[1]);
@@ -192,6 +203,7 @@ int run_cli(FILE *in, FILE *out)
             }
             printer->status = PRINTER_DISABLED;
             sf_printer_status(printer->name, PRINTER_DISABLED);
+
         } else if (strcmp(*array, "enable") == 0) {
             CHECK_ARG(length, 1);
             // pid_t pid;
@@ -204,18 +216,6 @@ int run_cli(FILE *in, FILE *out)
             printer->status = PRINTER_IDLE;
             sf_printer_status(printer->name, PRINTER_IDLE);
 
-            // int printer_mask = 1 << (printer-printers);
-            // for (int i = 0; i < MAX_JOBS; i++) {
-            //     if (((job_count >> i) & 0x1) && ((jobs[i].eligible & printer_mask) != 0) && jobs[i].status == JOB_CREATED && printers[i].status == PRINTER_IDLE) {
-            //         job_process_count++;
-            //         jobs_done = 1;
-            //         pid_t job = start_job(printer, &jobs[i]);
-            //         printer_pids[printer-printers] = job;
-            //         job_pids[i] = job;
-            //         // int scan_status;
-            //         // waitpid(job, &scan_status, 0);
-            //     }
-            // }
         } else {
             printf("Unrecognized command: %s\n", *array);
             sf_cmd_error("unrecognized command");
