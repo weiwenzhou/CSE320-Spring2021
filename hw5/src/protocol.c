d4ee2d408c9d1082921ef6840dea558a143de19f
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-05-05 21:08:59 +0000

    wrote proto_recv_packet

--- hw5/src/protocol.c
+++ hw5/src/protocol.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <unistd.h>
 
 #include "protocol.h"
@@ -35,11 +36,43 @@ int proto_send_packet(int fd, CHLA_PACKET_HEADER *hdr, void *payload) {
 }
 
 int proto_recv_packet(int fd, CHLA_PACKET_HEADER *hdr, void **payload) {
+    uint32_t header_size, payload_size, amount_read;
+    void *payload_buffer;
+    char *current_buffer;
     // read the header (keep track of partial reads)
+    header_size = sizeof(CHLA_PACKET_HEADER);
+    current_buffer = hdr;
+    while (header_size != 0) {
+        amount_read = write(fd, current_buffer, header_size);
+        if (amount_read <= 0) { // write error or wrote 0 byte 
+            return -1;
+        }
+        header_size -= amount_read;
+        current_buffer += amount_read;
+    }
 
     // get the payload size (convert from network byte order using ntohl)
+    payload_size = ntohl(hdr->payload_length);
+    // if the payload is non-zero create a buffer of size payload
+    if (payload_size) {
+        payload_buffer = malloc(payload_size);
+        if (payload_buffer == NULL) { // malloc error
+            return -1;
+        }
+    }
 
     // read the payload (keep track of partial reads)
-
+    current_buffer = payload_buffer;
+    while (payload_size != 0) {
+        amount_read = write(fd, current_buffer, payload_size);
+        if (amount_read <= 0) { // read error or read EOF
+            free(payload_buffer);
+            return -1;
+        }
+        payload_size -= amount_read;
+        current_buffer += amount_read;
+    }
+    // put payload buffer in payload pointer
+    *payload = payload_buffer;
     return 0;
 }
