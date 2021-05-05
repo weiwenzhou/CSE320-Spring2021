24b6b4c79863cb0287a03820759fd8afcf13816d
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-05-05 20:48:49 +0000

    write out proto_send_packet

--- hw5/src/protocol.c
+++ hw5/src/protocol.c
@@ -1,11 +1,30 @@
+#include <unistd.h>
+
 #include "protocol.h"
 
 int proto_send_packet(int fd, CHLA_PACKET_HEADER *hdr, void *payload) {
+    uint32_t header_size, payload_size, amount_written;
     // get the payload size (convert from network byte order using ntohl)
+    payload_size = ntohl(hdr->payload_length);
 
     // write the header (keep track of partial writes)
+    header_size = sizeof(CHLA_PACKET_HEADER);
+    while (header_size != 0) {
+        amount_written = write(fd, hdr, header_size);
+        if (amount_written <= 0) { // write error or wrote 0 byte 
+            return -1;
+        }
+        header_size -= amount_written;
+    }
 
     // write the payload (keep track of partial writes)
+    while (payload_size != 0) {
+        amount_written = write(fd, payload, payload_size);
+        if (amount_written <= 0) { // write error or wrote 0 bytes
+            return -1;
+        }
+        payload_size -= amount_written;
+    }
 
     return 0;
 }
