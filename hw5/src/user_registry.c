32ad43ae30db513fef9c7ef1eac27e6c56bc28f9
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-05-06 04:27:32 +0000

    layout function structure

--- hw5/src/user_registry.c
+++ hw5/src/user_registry.c
@@ -11,17 +11,33 @@ typedef struct user_registry {
 
 USER_REGISTRY *ureg_init(void) {
     // initialize the head of doubly linked list
+    // malloc a user registry
+    // malloc a mutex
+    // init mutex
+    // have prev and next point to itself
+    // user is NULL;
     return NULL;
 }
 
 void ureg_fini(USER_REGISTRY *ureg) {
-
+    // for each USER_REGISTRY 
 }
 
 USER *ureg_register(USER_REGISTRY *ureg, char *handle) {
+    // check if the handle is register
+    // if yes then return the user
+    // else
+    // create a new user
+    // create a new user registry and add to the end of the list
+
+    // do for both cases
+    // increase user reference count for the USER * returned
     return NULL;
 }
 
 void ureg_unregister(USER_REGISTRY *ureg, char *handle) {
+    // check if the handle is register
+    // if yes then
+    // decrement ref count on user and remove user_registry
 
 }
