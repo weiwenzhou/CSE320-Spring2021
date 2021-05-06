036d48abe1b3615c956a31b835cba86119df0bfc
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-05-06 19:08:09 +0000

    add function headers

--- hw5/src/client_registry.c
+++ hw5/src/client_registry.c
@@ -3,9 +3,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "client_registry.h"
+#include "client_registry.h" 
+#include "globals.h"
 
 typedef struct client_registry {
+    int count; // number of clients registered
     CLIENT *client[MAX_CLIENTS]; 
     pthread_mutex_t *mutex; // mutex for thread-safe operations
 } CLIENT_REGISTRY;
+
+CLIENT_REGISTRY *creg_init() {
+    return NULL;
+}
+
+void creg_fini(CLIENT_REGISTRY *cr) {
+    
+}
+
+CLIENT *creg_register(CLIENT_REGISTRY *cr, int fd) {
+    return NULL;
+}
+
+int creg_unregister(CLIENT_REGISTRY *cr, CLIENT *client) {
+    return 0;
+}
+
+CLIENT **creg_all_clients(CLIENT_REGISTRY *cr) {
+    return NULL;
+}
+
+void creg_shutdown_all(CLIENT_REGISTRY *cr) {
+
+}
