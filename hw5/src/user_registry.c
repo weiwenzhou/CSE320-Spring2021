76320cbfce1ec0ef976521354ba817a411e0af49
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-05-06 04:47:40 +0000

    wrote ureg register

--- hw5/src/user_registry.c
+++ hw5/src/user_registry.c
@@ -1,15 +1,17 @@
 #include <errno.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "debug.h"
+#include "user.h"
 #include "user_registry.h"
 
 typedef struct user_registry {
     USER *user;
     pthread_mutex_t *mutex; // mutex for thread-safe
-    USER_REGISTRY* prev; // links for doubly linked list
-    USER_REGISTRY* next;
+    USER_REGISTRY *prev; // links for doubly linked list
+    USER_REGISTRY *next;
 } USER_REGISTRY;
 
 USER_REGISTRY *ureg_init(void) {
@@ -34,15 +36,34 @@ void ureg_fini(USER_REGISTRY *ureg) {
 }
 
 USER *ureg_register(USER_REGISTRY *ureg, char *handle) {
-    // check if the handle is register
-    // if yes then return the user
-    // else
-    // create a new user
-    // create a new user registry and add to the end of the list
-
-    // do for both cases
-    // increase user reference count for the USER * returned
-    return NULL;
+    pthread_mutex_lock(ureg->mutex);
+    USER_REGISTRY *head, *current;
+    head = ureg;
+    current = ureg;
+    while (current->next != head) {
+        current = current->next;
+        if (strcmp(user_get_handle(current->user), handle) == 0) { // found handle
+            user_ref(current->user, "ureg_register return existing user");
+            return current->user;
+        }
+    }
+    USER *user = user_create(handle);
+    if (user == NULL) {
+        return NULL;
+    }
+    USER_REGISTRY *new = malloc(sizeof(USER_REGISTRY));
+    if (new == NULL) {
+        user_unref(user, "ureg_register malloc failed"); // free user
+        return NULL;
+    }
+    new->user = user;
+    ureg->prev->next = new;
+    new->prev = ureg->prev;
+    new->next = ureg;
+    ureg->prev = new;
+    user_ref(user, "ureg_register return new user");
+    pthread_mutex_unlock(ureg->mutex);
+    return user;
 }
 
 void ureg_unregister(USER_REGISTRY *ureg, char *handle) {
