def3300226f9753b92539c3b229cb6cf75bf0903
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-05-06 06:00:02 +0000

    add function headers

--- hw5/src/client.c
+++ hw5/src/client.c
@@ -19,3 +19,47 @@ typedef struct client {
     int referenceCount; // number of references
     pthread_mutex_t *mutex; // mutex for thread-safe operations
 } CLIENT;
+
+CLIENT *client_create(CLIENT_REGISTRY *creg, int fd) {
+    return NULL;
+}
+
+CLIENT *client_ref(CLIENT *client, char *why) {
+    return client;
+}
+
+void client_unref(CLIENT *client, char *why) {
+
+}
+
+int client_login(CLIENT *client, char *handle) {
+    return 0;
+}
+
+int client_logout(CLIENT *client) {
+    return 0;
+}
+
+USER *client_get_user(CLIENT *client, int no_ref) {
+    return client->user;
+}
+
+MAILBOX *client_get_mailbox(CLIENT *client, int no_ref) {
+    return client->mailbox;
+}
+
+int client_get_fd(CLIENT *client) {
+    return client->fd;
+}
+
+int client_send_packet(CLIENT *user, CHLA_PACKET_HEADER *pkt, void *data) {
+    return 0;
+}
+
+int client_send_ack(CLIENT *client, uint32_t msgid, void *data, size_t datalen) {
+    return 0;
+}
+
+int client_send_nack(CLIENT *client, uint32_t msgid) {
+    return 0;
+}
