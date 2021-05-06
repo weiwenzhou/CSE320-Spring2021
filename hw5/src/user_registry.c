e9b9bd1b28424e524445cf1666e741e9ba22aa42
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-05-06 04:32:29 +0000

    wrote ureg init

--- hw5/src/user_registry.c
+++ hw5/src/user_registry.c
@@ -1,22 +1,32 @@
+#include <errno.h>
 #include <pthread.h>
+#include <stdlib.h>
 
+#include "debug.h"
 #include "user_registry.h"
 
 typedef struct user_registry {
     USER *user;
-    pthread_mutex_t mutex; // mutex for thread-safe
+    pthread_mutex_t *mutex; // mutex for thread-safe
     USER_REGISTRY* prev; // links for doubly linked list
     USER_REGISTRY* next;
 } USER_REGISTRY;
 
 USER_REGISTRY *ureg_init(void) {
     // initialize the head of doubly linked list
-    // malloc a user registry
-    // malloc a mutex
-    // init mutex
-    // have prev and next point to itself
-    // user is NULL;
-    return NULL;
+    USER_REGISTRY *head = malloc(sizeof(USER_REGISTRY));
+    pthread_mutex_t *mutex = malloc(sizeof(pthread_mutex_t));
+    if ((errno = pthread_mutex_init(mutex, NULL)) != 0) { // error
+        free(head);
+        free(mutex);
+        return NULL;
+    }
+    head->mutex = mutex;
+    head->prev = head;
+    head->next = head;
+    head->user = NULL;
+    info("Initialize user registry");
+    return head;
 }
 
 void ureg_fini(USER_REGISTRY *ureg) {
