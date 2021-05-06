c7f11cf5bf5c9e83fdcf746ef53d95124b42b78e
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-05-06 19:22:43 +0000

    layout function logic

--- hw5/src/client_registry.c
+++ hw5/src/client_registry.c
@@ -34,18 +34,35 @@ CLIENT_REGISTRY *creg_init() {
 }
 
 void creg_fini(CLIENT_REGISTRY *cr) {
-    
+    // lock mutex
+    // loop through array and free every non null entry until count is 0
+    // unlock mutex
 }
 
 CLIENT *creg_register(CLIENT_REGISTRY *cr, int fd) {
+    // lock mutex
+    // if count < max_client
+        // create client
+        // add to next free spot in array
+        // increment count
+    // unlock mutex
     return NULL;
 }
 
 int creg_unregister(CLIENT_REGISTRY *cr, CLIENT *client) {
+    // lock mutex
+    // search for client
+    // if found unref and remove from array decrement count
+    // else error
+    // unlock mutex
     return 0;
 }
 
 CLIENT **creg_all_clients(CLIENT_REGISTRY *cr) {
+    // lock mutex
+    // calloc an array of size count + 1 (for null)
+    // client_ref each pointer
+    // unlock mutex
     return NULL;
 }
 
