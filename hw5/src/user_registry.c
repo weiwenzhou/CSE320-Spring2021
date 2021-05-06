f5fc1751fcd5f1c93689ebc63d4dd9651ff6514c
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-05-06 03:59:51 +0000

    add ureg function headers

--- hw5/src/user_registry.c
+++ hw5/src/user_registry.c
@@ -8,3 +8,20 @@ typedef struct user_registry {
     USER_REGISTRY* prev; // links for doubly linked list
     USER_REGISTRY* next;
 } USER_REGISTRY;
+
+USER_REGISTRY *ureg_init(void) {
+    // initialize the head of doubly linked list
+    return NULL;
+}
+
+void ureg_fini(USER_REGISTRY *ureg) {
+
+}
+
+USER *ureg_register(USER_REGISTRY *ureg, char *handle) {
+    return NULL;
+}
+
+void ureg_unregister(USER_REGISTRY *ureg, char *handle) {
+
+}
