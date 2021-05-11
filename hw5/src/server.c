d9c86364f61cfdf18804bdce1c9ddddd13b95524
Wei Wen Zhou <weiwezhou@cs.stonybrook.edu>
2021-05-11 00:33:39 +0000

    wrote mailbox service

--- hw5/src/server.c
+++ hw5/src/server.c
@@ -17,30 +17,52 @@ void discard_hook(MAILBOX_ENTRY *entry) {
 }
 
 void *chla_mailbox_service(void *arg) {
-    // pass client
-    // get client (increase ref)
-    // get mailbox of client client_get_mailbox(,0)
+    CLIENT *client = client_ref((CLIENT *)arg, "for reference being retained by mailbox service thread");
+    MAILBOX *mailbox = client_get_mailbox(client, 0);
+    MAILBOX_ENTRY *entry;
 
     // if mailbox is null???? exit
 
-    // set discard hook
-    // while loop with mb_next entry until null
-        // packet header 
-        // message entry type or notice entry type
-            // message send to client_send_packet
-            // send ack to sender
-            
-            // notice entry type 
-            // BOUNCE_NOTICE_TYPE
-                // send bounce to sender
-            // RRCPT_NOTICE_TYPE
-                // send to sender   
-
-    // terminate
-    // unref client
-    // mb unref 
-    // pthread exit
-    return NULL;
+    mb_set_discard_hook(mailbox, discard_hook);
+    while ((entry = mb_next_entry(mailbox)) != NULL) {
+        CHLA_PACKET_HEADER packet;
+        if (entry->type == MESSAGE_ENTRY_TYPE) {
+            packet.msgid = entry->content.message.msgid;
+            struct timespec timestamp;
+            if (clock_gettime(CLOCK_REALTIME, &timestamp) == -1) { // error
+                goto bad_message;
+            }
+            packet.payload_length = htonl(entry->content.message.length);
+            packet.timestamp_sec = htonl(timestamp.tv_sec);
+            packet.timestamp_nsec = htonl(timestamp.tv_nsec);
+            int status = client_send_packet(client, &packet, entry->content.message.body);
+            if (status == 0) {
+                mb_add_notice(entry->content.message.from, RRCPT_NOTICE_TYPE, entry->content.message.msgid);
+            } else {
+                bad_message:
+                mb_add_notice(entry->content.message.from, BOUNCE_NOTICE_TYPE, entry->content.message.msgid);
+            }
+        } else { // notice entry type
+            packet.msgid = entry->content.notice.msgid;
+            struct timespec timestamp;
+            if (clock_gettime(CLOCK_REALTIME, &timestamp) == -1) { // error
+                goto bad_message;
+            }
+            packet.payload_length = 0;
+            packet.timestamp_sec = htonl(timestamp.tv_sec);
+            packet.timestamp_nsec = htonl(timestamp.tv_nsec);
+            if (entry->content.notice.type == BOUNCE_NOTICE_TYPE) {
+                packet.type = CHLA_BOUNCE_PKT;
+            } else {
+                packet.type = CHLA_RCVD_PKT;
+            }
+            client_send_packet(client, &packet, NULL);
+        }
+        free(entry);
+    }
+    mb_unref(mailbox, "for reference being discarded by terminating mailbox service");
+    client_unref(client, "for reference being discarded by terminating mailbox service thread");
+    pthread_exit(NULL);
 }
 
 void *chla_client_service(void *arg) {
@@ -65,8 +87,9 @@ void *chla_client_service(void *arg) {
                 // parse string payload
                 status = client_login(self, handle);
                 if (status == 0) { // success
-                    pthread_create(&mailbox_tid, NULL, chla_mailbox_service, self);
                     client_send_ack(self, packet.msgid, NULL, 0);
+                    success("Starting mailbox service for: %s (fd=%d)", user_get_handle(client_get_user(self, 1)), connfd);
+                    pthread_create(&mailbox_tid, NULL, chla_mailbox_service, self);
                 } else {
                     client_send_nack(self, packet.msgid);
                 }
@@ -74,13 +97,11 @@ void *chla_client_service(void *arg) {
                 break;
             
             case CHLA_LOGOUT_PKT:
-                /* code */
                 success("LOGOUT");
                 status = client_logout(self);
                 if (status == 0) {
                     client_send_ack(self, packet.msgid, NULL, 0);
                     success("Waiting for mailbox service thread (%ld) to terminate", mailbox_tid);
-                    pthread_join(mailbox_tid, NULL);
                 } else {
                     client_send_nack(self, packet.msgid);
                 }
@@ -100,39 +121,28 @@ void *chla_client_service(void *arg) {
                         payload_length += strlen(user_get_handle(temp)) + 2;
                 }
                 if (payload_length > 0) {
-                    send_payload = malloc(payload_length-1);
+                    send_payload = malloc(payload_length+1);
                     for (int i = 0; clients[i] != NULL; i++) {
                         // copy handles
                         temp = client_get_user(clients[i], 1);
                         if (temp != NULL) {
-                            if (i != 0)
-                                strcat(send_payload, "\r\n");
                             strcat(send_payload, user_get_handle(temp));
+                            strcat(send_payload, "\r\n");
                         }
                         client_unref(clients[i], "for reference in clients list being discarded");
                     }
                     free(clients);
-                    sender.payload_length = payload_length-1;
+                    client_send_ack(self, packet.msgid, payload_length, send_payload);
                 } else {
-                    sender.payload_length = 0;
+                    client_send_ack(self, packet.msgid, NULL, 0);
                 }
-                sender.msgid = packet.msgid;
-                struct timespec timestamp;
-                if (clock_gettime(CLOCK_REALTIME, &timestamp) == -1) { // error
-                    end = 1;
-                    break;
-                }
-                sender.timestamp_sec = htonl(timestamp.tv_sec);
-                sender.timestamp_nsec = htonl(timestamp.tv_nsec);
-                sender.type = CHLA_USERS_PKT;
-                client_send_packet(self, &sender, send_payload);
                 free(send_payload);
                 break;
 
             case CHLA_SEND_PKT:
                 /* code */
                 success("SEND");
-                // client_send_packet()
+                // add message
                     // client_send_ack
                 // free payload
                 break;
